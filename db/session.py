6abc6a45326c6c91264afe22a966859d5cf58018
Nick Nyanjui <nicknyanjui@gmail.com>
2023-07-14 13:07:24 +0300

    Implement a generator to query tables using dependecy injection

--- db/session.py
+++ db/session.py
@@ -2,9 +2,20 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Generator
 from core.config import settings
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+print(f"Database URL: {SQLALCHEMY_DATABASE_URL}")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SESSION_LOCAL = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db() -> Generator:
+    """"""
+    try:
+        db = SESSION_LOCAL()
+        yield db
+    finally:
+        db.close()
