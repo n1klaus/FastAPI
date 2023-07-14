755af23b3d7234f4d4ca1a569476133d95d8fc7f
Nick Nyanjui <nicknyanjui@gmail.com>
2023-07-14 13:20:17 +0300

    Implement hashing with passlib and bcrypt

---
+++ core/hashing.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Hasher:
+    """Class to handle passwords"""
+
+    @staticmethod
+    def verify(plain_password, hashed_password):
+        """Verifies provided plain password string with hashed password"""
+        return pwd_context.verify(plain_password, hashed_password)
+    
+    @staticmethod
+    def get(plain_password):
+        """Returns hashed form of the plain password from the string"""
+        return pwd_context.hash(plain_password)
