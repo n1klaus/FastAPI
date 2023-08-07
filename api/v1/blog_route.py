984f2cb829a8e29ff6291486fc3ab8142fc4aafe
Nick Nyanjui <nicknyanjui@gmail.com>
2023-08-07 13:18:57 +0300

    Add blog retrieval functionality

--- api/v1/blog_route.py
+++ api/v1/blog_route.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy.orm import Session
-from fastapi import Depends
 
 from db.session import get_db
 from schemas.blog import BlogCreate, BlogView
-from db.repository.blog import create_new_blog
+from db.repository.blog import create_new_blog, retrieve_blog
 
 router = APIRouter()
 
 @router.post("/blogs", reponse_model=BlogView, status_code=status.HTTP_201_CREATED)
-async def create_blog(blog: BlogCreate, db: Session = Depends(get_db))
+def create_blog(blog: BlogCreate, db: Session = Depends(get_db)):
+    """Creates and returns the newly created blog"""
     blog = create_new_blog(blog-blog, db=db, author_id=1)
+    return blog
+
+@router.get("blogs/{id}", response_model=BlogView)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    """Returns an existing blog with the given id"""
+    blog = retrieve_blog(id=id, db=db)
+    if not blog:
+        raise HTTPException(detail=f"Blog with ID {id} does not exist", status_code=status.HTTP_404_NOT_FOUND)
     return blog
--- db/repository/blog.py
+++ db/repository/blog.py
@@ -5,8 +5,14 @@ from schemas.blog import BlogCreate
 from db.models.blog import Blog
 
 def create_new_blog(blog: BlogCreate, db: Session, author_id: int = 1):
+    """Creates a new blog"""
     blog = Blog(**blog.dict(), author_id = author_id)
     db.add(blog)
     db.commit()
     db.refresh(blog)
+    return blog
+
+def retrieve_blog(id: int, db: Session):
+    """Retrieves an existing blog"""
+    blog = db.query(Blog).filter(Blog.id == id).first()
     return blog
