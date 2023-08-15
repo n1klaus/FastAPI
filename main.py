c8c34a1ac156b300cbf5044baeb1c73f9f8d9cd2
Nick Nyanjui <nicknyanjui@gmail.com>
2023-08-16 01:28:23 +0300

    Update main app file: Add uvicorn setup

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 
 from fastapi import FastAPI
 from db.session import engine
-from db.base import Base
-from core.config import settings
+from db.base import BaseClass as Base
+from core.config import get_settings
 from api.base import api_router
 
+settings = get_settings()
+
 def create_tables():
-    """"""
+    """Create all tables needed by the application"""
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
 def include_routers(app: FastAPI):
@@ -15,7 +18,7 @@ def include_routers(app: FastAPI):
 
 
 def start_app():
-    """"""
+    """Start the FastAPI application"""
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     create_tables()
     include_routers(app)
@@ -27,3 +30,6 @@ app = start_app()
 def hello_api():
     return {'msg': 'Hello FastAPI🚀'}
 
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app='main:app', host='0.0.0.0', port=8000, reload=True, log_level="debug")
